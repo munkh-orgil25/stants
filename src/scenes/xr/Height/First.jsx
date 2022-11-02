2af365a8198fe30d01c1eb70c2e2e0e155952df1
munkh-orgil25 <orgilmedq@gmail.com>
2022-11-03 00:53:11 +0800

    test: pos info

--- src/scenes/xr/Height/First.jsx
+++ src/scenes/xr/Height/First.jsx
@@ -1,32 +1,23 @@
 import { Interactive, useXR } from '@react-three/xr'
 import { useEffect, useRef, useState } from 'react'
 import { a, useSpring, config } from '@react-spring/three'
+import { useFrame } from '@react-three/fiber'
 import HoverButton from '../../../components/HoverButton'
 import InfoText from '../../../components/InfoText'
 
 export default function First({ setLocation }) {
   const infoRef = useRef()
-  const [infoVisible, setInfoVisible] = useState(false)
+  const [infoVisible, setInfoVisible] = useState(true)
+  const [camera, setCamera] = useState(null)
   const { player, isPresenting } = useXR()
-
-  useEffect(() => {
-    if (isPresenting) {
-      player.children[0].add(infoRef.current)
-    }
-
-    return () => {
-      player.children[0].children.length = 0
-    }
-  }, [isPresenting])
 
   return (
     <group>
       {/* INFO */}
-      <group ref={infoRef} position={[0, 0, -2]}>
+      <group position={[-4, 0.6, -5]} rotation={[0, 0.75, 0]}>
         <HoverButton
           position={[0, 0, -0.01]}
-          rotation={[Math.PI * 0.5, Math.PI * 0.5, 0]}
-          scale={0.15}
+          scale={0.5}
           text="Унших"
           onClick={() => setInfoVisible(true)}
         />
@@ -35,10 +26,9 @@ export default function First({ setLocation }) {
             visible={infoVisible}
             onClick={() => setInfoVisible(false)}
             scale={1}
-            position={[0, 0, 0]}
-            rotation={[Math.PI * 0.5, Math.PI * 0.5, 0]}
+            position={[0, 1, 4]}
             title="ХХБ дахь цахилгааны аюул."
-            text="Цахилгааны Хаалттай хувиарлах байгууламж ( ХХБ) нь цахилгаан тоноглолын таслах,залгах аппаратур , удирдлагын хэлхээ байралдаг сэлгэн залгалт хийгддэг онц чухал объект юм."
+            text="1,3 метрээс дээш өндөрт зориулалтын (нэмэлт бэхлэгээ хийгдсэн, пайзтай, шат тавцанг акт үйлдэж хүлээлцсэн гэх мэт) шат тавьсан дээр ажил гүйцэтгэх ёстой. Та өндөр ажил гүйцэтгэх үед өнөөдрийн бүсийг тогтмол зүүж амин цэг, амин олсноос бүсийг бэхлэж ажил гүйцэтгэх ёстой. Өндөр .  Биё биендээ хяналт тавьж, өндөөр биет унагаахгүй болгоомжтой ажил гүйцэтгэх."
           />
         </Interactive>
       </group>
